9dee50def03e7345a3f01da4a7e1a49b9e993191
H1klo <nikitos.2k4@gmail.com>
2023-10-13 14:33:09 +0200

    Lesson2

--- JavaLessons/src/FirstClass.java
+++ JavaLessons/src/FirstClass.java
@@ -1,9 +1,21 @@
+import java.util.Random;
+
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+//        float mass = 83.0f;
+//        double marsMass = mass*0.38f;
+//        System.out.printf("%.1f kg on Earth is kg %.4f on Mars\n", mass,marsMass);
+//        int marsMassInt = (int)marsMass;
+//        System.out.println("Mars mass in round to int: "+ marsMassInt);
+//        char marsMassChar = (char)marsMassInt;
+//        System.out.println("Mars mass in round to char: "+ marsMassChar);
+//        int marsMassCharOperations = marsMassChar * 2 -3;
+//        System.out.println("Char after operations: "+ marsMassCharOperations);
 
-        System.out.println("Hello World");
-        System.out.println("Hello World");
 
+//      int randomNumber = (int) (Math.random()*100);
+//
+//      System.out.println(randomNumber + "\nThe number is even? : \n" + (randomNumber % 2==0));
+        
     }
 }
